53aac1cc8d916144a4cdc0c887ce4edd0bb5f40d
Xiaoran Weng <goose_bomb@Yunshans-Gadget.local>
2020-08-17 22:31:09 +0800

    add edge-aware filtering example

---
+++ python/edge_aware_filters.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    img_src = cv2.imread('./images/trek.png')
+    img_src = cv2.cvtColor(img_src, cv2.COLOR_BGR2RGB)
+
+    img_sketch, img_sketch = cv2.pencilSketch(
+        img_src, None, None, 50, 0.08, 0.05)
+    img_skin_smoothed = cv2.edgePreservingFilter(
+        img_src, None, cv2.RECURS_FILTER, 50, 0.1)
+    img_detail_enhanced = cv2.detailEnhance(img_src, None, 50, 0.06)
+
+    fig, axs = plt.subplots(2, 2, num='Edge aware filtering', figsize=(8, 8))
+    axs[0, 0].imshow(img_src)
+    axs[0, 1].imshow(img_sketch)
+    axs[1, 0].imshow(img_skin_smoothed)
+    axs[1, 1].imshow(img_detail_enhanced)
+
+    axs[0, 0].set_title('Original')
+    axs[0, 1].set_title('Pencil sketch')
+    axs[1, 0].set_title('Edge-preserving smooth')
+    axs[1, 1].set_title('Detail enhance')
+
+    plt.show()
