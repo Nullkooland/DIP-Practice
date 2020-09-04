08d6a1b97aa8bb6c30e37942554e2537bea679a0
Goose-Bomb <goose_bomb@outlook.com>
2020-09-04 23:01:08 +0800

    add fit line and pixel stylize example

---
+++ python/fit_line.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.cm import get_cmap
+
+SLOPE = 1.25
+Y_OFFSET = -0.5
+NUM_POINTS = 100
+NOISE_STD = 0.25
+OUTLIER_Y_OFFSET = 5.0
+
+FIT_DIST_TYPES = [
+    ('L1', cv2.DIST_L1),
+    ('L2', cv2.DIST_L2),
+    ('L12', cv2.DIST_L12),
+    ('FAIR', cv2.DIST_FAIR),
+    ('WELSCH', cv2.DIST_WELSCH),
+    ('HUBER', cv2.DIST_HUBER),
+]
+
+if __name__ == "__main__":
+    x= np.random.rand(NUM_POINTS)
+    n= np.random.randn(NUM_POINTS) * NOISE_STD
+    y= SLOPE * x + Y_OFFSET + n
+
+    outlier_index= np.random.randint(0, NUM_POINTS)
+    y[outlier_index] += OUTLIER_Y_OFFSET
+    observations= np.stack((x, y), axis=1)
+
+    plt.figure('Line fitting', figsize=(8, 8))
+    # show observations
+    plt.plot(x, y, 'o', color='black')
+    # highlight outlier
+    plt.plot(x[outlier_index], y[outlier_index], 'o', color='red')
+    # draw groundtruth line
+    plt.axline((0, Y_OFFSET), slope=SLOPE,
+               linestyle='--', linewidth=2, color='gray', label='groundtruth')
+               
+    cmap = get_cmap('tab10')
+
+    for i, (dist_type_name, dist_type) in enumerate(FIT_DIST_TYPES):
+        line=cv2.fitLine(observations, dist_type, 0, 0.01, 0.01)
+        vx, vy, x0, y0=line.squeeze()
+        plt.axline((x0, y0), slope=vy / vx, label=dist_type_name, color=cmap.colors[i])
+
+    plt.ylim(np.min(y) * 1.5, np.max(y) * 1.5)
+    plt.legend()
+    plt.show()
---
+++ python/pixel_stylize.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+PIXEL_SIZE = 4
+PIXEL_MARGIN = 2
+
+if __name__ == "__main__":
+    img_src = cv2.imread('./images/testcard.png')
+    img_src = cv2.cvtColor(img_src, cv2.COLOR_BGR2RGB)
+
+    h, w = img_src.shape[:2]
+
+    img_stylized = np.zeros_like(img_src)
+    mask = np.zeros((h, w), dtype=np.uint8)
+
+    # form pixel-like style image
+    for i in range(0, h - PIXEL_MARGIN, PIXEL_SIZE + PIXEL_MARGIN):
+        for j in range(0, w - PIXEL_MARGIN, PIXEL_SIZE + PIXEL_MARGIN):
+            mask[i + PIXEL_MARGIN:i + PIXEL_MARGIN +
+                 PIXEL_SIZE, j + PIXEL_MARGIN: j + PIXEL_MARGIN + PIXEL_SIZE] = 255
+
+            roi_src = img_src[i + PIXEL_MARGIN:i + PIXEL_MARGIN +
+                              PIXEL_SIZE, j + PIXEL_MARGIN: j + PIXEL_MARGIN + PIXEL_SIZE]
+
+            roi_dst = img_stylized[i + PIXEL_MARGIN:i + PIXEL_MARGIN +
+                                   PIXEL_SIZE, j + PIXEL_MARGIN:j + PIXEL_MARGIN + PIXEL_SIZE]
+
+            roi_dst[:] = cv2.mean(roi_src)[:3]
+
+    # add glow effects
+    layer_glow = cv2.GaussianBlur(img_stylized, (9, 9), 1.5)
+    img_stylized += cv2.add(img_stylized, layer_glow, mask=~mask)
+
+    fig, (ax_src, ax_stylized) = plt.subplots(
+        1, 2, num='Pixel Stylized', figsize=(12, 6))
+    ax_src.imshow(img_src)
+    ax_src.set_title('Original')
+
+    ax_stylized.imshow(img_stylized)
+    ax_stylized.set_title('Stylized')
+
+    plt.show()
