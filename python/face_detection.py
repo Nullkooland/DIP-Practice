e63abac059daf5d37248e8559426409f3017c995
Goose-Bomb <goose_bomb@outlook.com>
2020-09-06 13:58:05 +0800

    man the cascade eye detector is crappy

--- python/face_detection.py
+++ python/face_detection.py
@@ -1,7 +1,10 @@
 import numpy as np
 import cv2
 
-FEATURE_DESC_FILE = '/usr/local/share/opencv4/haarcascades/haarcascade_frontalface_alt.xml'
+FEATURES_FILES_DIR = '/usr/local/share/opencv4/haarcascades/'
+FACE_FEATURES_FILE = 'haarcascade_frontalface_alt.xml'
+EYE_FEATURES_FILE = 'haarcascade_eye.xml'
+SMILE_FEATURES_FILE = 'haarcascade_smile.xml'
 
 # Prepare this shitty 720P camera on my MBP
 cap = cv2.VideoCapture(1)
@@ -9,8 +12,11 @@ w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 print(f'Video input size:{w}x{h}')
 
-# Prepare the detector
-detector = cv2.CascadeClassifier(FEATURE_DESC_FILE)
+# Prepare the detectors
+face_detector = cv2.CascadeClassifier(FEATURES_FILES_DIR + FACE_FEATURES_FILE)
+eye_detector = cv2.CascadeClassifier(FEATURES_FILES_DIR + EYE_FEATURES_FILE)
+smile_detector = cv2.CascadeClassifier(
+    FEATURES_FILES_DIR + SMILE_FEATURES_FILE)
 
 background = cv2.imread('./images/loquat_painting.png')
 bg_height, bg_width, _ = background.shape
@@ -31,24 +37,22 @@ while (True):
     gray = cv2.GaussianBlur(gray, (7, 7), 1.1)
 
     # Get detected face regions
-    faces = detector.detectMultiScale(gray,
-                                      scaleFactor=1.08,
-                                      minNeighbors=5,
-                                      flags=cv2.CASCADE_SCALE_IMAGE,
-                                      minSize=(100, 100),
-                                      maxSize=(600, 600))
+    faces = face_detector.detectMultiScale(gray,
+                                           scaleFactor=1.2,
+                                           minNeighbors=5,
+                                           flags=cv2.CASCADE_FIND_BIGGEST_OBJECT,
+                                           minSize=(100, 100), maxSize=(600, 600))
 
     for (x, y, w, h) in faces:
-        h_ex = h // 4
-        h_ex = min(h_ex, y)
-        h_roi = min(h + h_ex * 2, bg_height)
-        w_roi = min(w, bg_width)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-        # roi = frame[y - h_ex:y - h_ex + h_roi, x:x + w_roi]
-        # pos = (bg_width // 2, h_roi // 2)
+        roi_face = frame[y:y + h, x:x + w]
+        roi_face_gray = gray[y:y + h, x:x + w]
+        eyes = eye_detector.detectMultiScale(roi_face_gray)
 
-        # blend = cv2.seamlessClone(roi, background, None, pos, cv2.MIXED_CLONE)
-        frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        for (x_e, y_e, w_e, h_e) in eyes:
+            cv2.rectangle(roi_face, (x_e, y_e),
+                          (x_e + w_e, y_e + h_e), (255, 255, 0), 2)
 
     tick.stop()
     fps = 1.0 / tick.getTimeSec()
