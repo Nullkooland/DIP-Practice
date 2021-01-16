05a22fe8e2184e6d21d9500f537b959bec9b91d8
Goose-Bomb <goose_bomb@outlook.com>
2021-01-16 20:32:28 +0800

    Add GMM bgsegm example

--- python/video_analysis/background_substraction.py
+++ python/video_analysis/background_substraction.py
@@ -2,18 +2,20 @@ import numpy as np
 import cv2
 
 if __name__ == "__main__":
-    cap = cv2.VideoCapture(1)
-    fgbg = cv2.createBackgroundSubtractorMOG2()
-    
+    cap = cv2.VideoCapture('./videos/freeway720.264')
+    # bg_subtractor = cv2.bgsegm.createBackgroundSubtractorGMG()
+    bg_subtractor = cv2.createBackgroundSubtractorMOG2(detectShadows=True, history=128)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
 
     while True:
-        ret, frame = cap.read()
-
-        fgmask = fgbg.apply(frame)
-        cv2.imshow('frame', fgmask)
-
-        if cv2.waitKey(30) == ord('q'):
+        is_read_ok, frame = cap.read()
+        if not is_read_ok or cv2.waitKey(16) & 0xFF == ord('q'):
             break
+
+        mask_fg = bg_subtractor.apply(frame)
+        mask_fg = cv2.morphologyEx(mask_fg, cv2.MORPH_OPEN, kernel)
+        cv2.imshow('frame', frame)
+        cv2.imshow('mask', mask_fg)
 
     cap.release()
     cv2.destroyAllWindows()
