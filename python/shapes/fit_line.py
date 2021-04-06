9dd10016af74e9acd55deff981e5bc113452b450
Goose-Bomb <goose_bomb@outlook.com>
2021-04-06 17:02:03 +0800

    Add more outliers for line fitting demo

--- python/shapes/fit_line.py
+++ python/shapes/fit_line.py
@@ -6,9 +6,11 @@ from matplotlib.cm import get_cmap
 
 SLOPE = 1.25
 Y_OFFSET = -0.5
-NUM_POINTS = 100
+NUM_POINTS = 128
 NOISE_STD = 0.25
-OUTLIER_Y_OFFSET = 5.0
+
+NUM_OUTLIERS = NUM_POINTS // 16
+OUTLIER_Y_MEAN_OFFSET = 2.5
 
 FIT_DIST_TYPES = [
     ("L1", cv2.DIST_L1),
@@ -20,30 +22,33 @@ FIT_DIST_TYPES = [
 ]
 
 if __name__ == "__main__":
-    x= np.random.rand(NUM_POINTS)
-    n= np.random.randn(NUM_POINTS) * NOISE_STD
-    y= SLOPE * x + Y_OFFSET + n
+    x = np.random.rand(NUM_POINTS)
+    x = np.sort(x)
+    n = np.random.randn(NUM_POINTS) * NOISE_STD
+    y = SLOPE * x + Y_OFFSET + n
 
-    outlier_index= np.random.randint(0, NUM_POINTS)
-    y[outlier_index] += OUTLIER_Y_OFFSET
-    observations= np.stack((x, y), axis=1)
+    outlier_indices = np.random.randint(0, NUM_POINTS // 4, NUM_OUTLIERS)
+    y[outlier_indices] += np.random.randn(NUM_OUTLIERS) * NOISE_STD + OUTLIER_Y_MEAN_OFFSET
+    observations = np.stack((x, y), axis=1)
 
     plt.figure("Line fitting", figsize=(8, 8))
     # show observations
-    plt.plot(x, y, "o", color="black")
+    plt.plot(x, y, "o", color="black", label="data")
     # highlight outlier
-    plt.plot(x[outlier_index], y[outlier_index], "o", color="red")
+    plt.plot(x[outlier_indices], y[outlier_indices],
+             "o", color="red", label="outlier")
     # draw groundtruth line
     plt.axline((0, Y_OFFSET), slope=SLOPE,
                linestyle="--", linewidth=2, color="gray", label="groundtruth")
-               
+
     cmap = get_cmap("tab10")
 
     for i, (dist_type_name, dist_type) in enumerate(FIT_DIST_TYPES):
-        line=cv2.fitLine(observations, dist_type, 0, 0.01, 0.01)
-        vx, vy, x0, y0=line.squeeze()
-        plt.axline((x0, y0), slope=vy / vx, label=dist_type_name, color=cmap.colors[i])
+        line = cv2.fitLine(observations, dist_type, 0, 0.01, 0.01)
+        vx, vy, x0, y0 = line.squeeze()
+        plt.axline((x0, y0), slope=vy / vx,
+                   label=f"Fit-{dist_type_name}", color=cmap.colors[i])
 
     plt.ylim(np.min(y) * 1.5, np.max(y) * 1.5)
-    plt.legend()
+    plt.legend(loc="upper right")
     plt.show()
