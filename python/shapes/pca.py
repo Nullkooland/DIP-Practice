bb9b051e9c1ca14cfc2e636dbbbb0d3b842d13d7
Goose-Bomb <goose_bomb@outlook.com>
2021-06-16 23:56:21 +0800

    Use matplotlib to draw contours and arrows

--- python/shapes/pca.py
+++ python/shapes/pca.py
@@ -1,50 +1,50 @@
 import cv2
+from matplotlib import pyplot as plt
 import pyheif
 import numpy as np
 
-src_img = pyheif.read_as_numpy("./images/rice.heic")
-cv2.imshow("rice", src_img)
+if __name__ == "__main__":
+    src_img = pyheif.read_as_numpy("./images/rice.heic")
 
-# binary threshold to get mask
-ret, mask = cv2.threshold(src_img[..., 0], 25, 255, cv2.THRESH_BINARY)
-# mask = cv2.adaptiveThreshold(src_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 9, 3)
+    # binary threshold to get mask
+    ret, mask = cv2.threshold(src_img[..., 0], 25, 255, cv2.THRESH_BINARY)
+    # mask = cv2.adaptiveThreshold(src_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 9, 3)
 
-# apply morphological filters to remove noises
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    # apply morphological filters to remove noises
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
 
-cv2.imshow("mask", mask)
+    # get contours
+    contours, hierarchy = cv2.findContours(
+        mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-# get contours
-contours, hierarchy = cv2.findContours(
-    mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_L1)
+    fig, ax = plt.subplots(1, 1, num="Rice", figsize=(12, 8))
+    ax.imshow(src_img)
 
-cv2.drawContours(src_img, contours, -1, (0, 0, 255), 1, lineType=cv2.LINE_AA)
+    # compute PCA
+    for contour in contours:
+        # Draw contours
+        contour = np.squeeze(contour, axis=1).astype(np.float32)
+        ax.fill(contour[:, 0], contour[:, 1], color="C1", fill=False)
 
-# compute PCA
-for contour in contours:
-    contour = np.squeeze(contour, axis=1).astype(np.float32)
-    center, directions, eigenvalues = cv2.PCACompute2(contour, np.empty(0))
-    # draw direction vectors for each rice seed
-    x, y = center[0]
-    v0 = directions[0]
-    v1 = directions[1]
+        center, directions, eigenvalues = cv2.PCACompute2(contour, np.empty(0))
+        # draw direction vectors for each rice seed
+        x, y = center[0]
+        v0 = directions[0]
+        v1 = directions[1]
 
-    len0 = np.dot(contour - center, v0)
-    len1 = np.dot(contour - center, v1)
+        len0 = np.dot(contour - center, v0)
+        len1 = np.dot(contour - center, v1)
 
-    v0 *= np.max(len0)
-    v1 *= np.max(len1)
+        v0 *= np.max(len0)
+        v1 *= np.max(len1)
 
-    cv2.circle(src_img, (x, y), 7, (0, 0, 255), 1, lineType=cv2.LINE_AA)
+        ax.plot(x, y, ls='', marker='o', markersize=8,
+                markerfacecolor="None", markeredgecolor="C3")
 
-    cv2.arrowedLine(src_img, (x, y), (x + v0[0], y + v0[1]),
-                    (0, 255, 0), 2, tipLength=0.25, line_type=cv2.LINE_AA)
+        ax.arrow(x, y, v0[0], v0[1], color="C0", width=1, head_width=6,
+                 head_length=6, length_includes_head=True)
+        ax.arrow(x, y, v1[0], v1[1], color="C2", width=0.5, head_width=3,
+                 head_length=3, length_includes_head=True)
 
-    cv2.arrowedLine(src_img, (x, y), (x + v1[0], y + v1[1]),
-                    (200, 75, 0), 2, tipLength=0.5, line_type=cv2.LINE_AA)
-
-
-cv2.imshow("rice contours", src_img)
-
-cv2.waitKey()
+    plt.show()
